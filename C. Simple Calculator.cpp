46767dae2349c23ba6f16c36ad89dc26d149b082
unknown <anirbanislamemon.online@gmail.com>
2025-08-17 03:06:02 +0600

    Multipies-In-C++

--- C. Simple Calculator.cpp
+++ C. Simple Calculator.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main() {
     int X, Y;
     cin >> X >> Y;
-
+if(1<=X && Y>=100000){
     cout << X << " + " << Y << " = " << X + Y << endl;
     cout << X << " * " << Y << " = " << X * Y << endl;
     cout << X << " - " << Y << " = " << X - Y << endl;
+}
+
 
     return 0;
 }
