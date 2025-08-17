74d15e05909dfb9ea6fcfc04d7f925701a1605ac
unknown <anirbanislamemon.online@gmail.com>
2025-08-18 02:19:10 +0600

    Letter-Check-C++

---
+++ M. Capital or Small or Digit.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cctype>   // for isdigit, isalpha, isupper, islower
+using namespace std;
+
+int main() {
+    char X;
+    cin >> X;
+
+    if (isdigit(X)) {
+        cout << "IS DIGIT" << endl;
+    } else if (isalpha(X)) {
+        cout << "ALPHA" << endl;
+        if (isupper(X)) {
+            cout << "IS CAPITAL" << endl;
+        } else {
+            cout << "IS SMALL" << endl;
+        }
+    }
+
+    return 0;
+}
+
